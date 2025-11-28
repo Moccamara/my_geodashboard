34ebef2c877983a00bdacc97154aa51a555c501e
Moccamara <mahamadoucamaramoc@outlook.com>
2025-11-28 14:41:53 +0000

    Update dashdoardmc.py

--- dashdoardmc.py
+++ dashdoardmc.py
@@ -128,7 +128,7 @@ folium.GeoJson(
 # CSV from server path
 # -----------------------------
 csv_folder = Path("data/csv")  # folder where CSVs are stored
-csv_file_name = "points.csv"   # CSV file to load
+csv_file_name = "Denombrement-B_DENOMBREMENT.csv"   # CSV file to load
 csv_path = csv_folder / csv_file_name
 
 points_gdf = None
@@ -172,3 +172,4 @@ st.markdown("""
 **Projet : Actualisation de la cartographie du RGPG5 (AC-RGPH5) – Mali**  
 Développé avec Streamlit sous Python par **CAMARA, PhD** • © 2025
 """)
+
